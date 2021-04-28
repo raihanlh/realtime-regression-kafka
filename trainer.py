d1d09f1ff70146f7a986418cd2c57ad27a6338bb
Raihan Luthfi Haryawan <raihan.luthfi.h@gmail.com>
2021-04-28 19:52:45 +0700

    add model log on trainer.py

--- trainer.py
+++ trainer.py
@@ -2,6 +2,7 @@ from kafka import KafkaConsumer
 import numpy as np
 from sklearn.linear_model import LinearRegression
 import pickle
+from datetime import datetime
 
 consumer = KafkaConsumer(
     'regression',
@@ -12,6 +13,8 @@ consumer = KafkaConsumer(
      value_deserializer=lambda x: x.decode('utf-8'))
 
 model = LinearRegression()
+i = 0
+dt_0 = datetime(2020, 1, 1)
 
 for message in consumer:
     message = message.value.split(";")
@@ -24,6 +27,8 @@ for message in consumer:
             y.append(float(pair[1]))
     
     model.fit(np.array(x).reshape(-1,1), np.array(y))
-    print(model.predict(np.array([[10],[20],[30]])))
-    
+    dt_i = datetime.fromtimestamp(dt_0.timestamp() + i * 86400)
+    print("Model {} dengan data tanggal {} s/d {} berhasil dibuat".format(i+1, dt_0.strftime('%Y-%m-%d'), dt_i.strftime('%Y-%m-%d')))
+    # print(model.predict(np.array([[10],[20],[30]])))
+    i += 1
     pickle.dump(model, open("reg_model.pickle", 'wb'))
